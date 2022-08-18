b62219266d3f2d8d96d63257d8f5ec70a4fed2ff
adkonte <alykonte19@gmail.com>
2022-08-18 12:43:50 +0000

    First commit

---
+++ src/test/java/com/tirage/API/Tirage/ApiTirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirage.API.Tirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
